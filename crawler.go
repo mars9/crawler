f8e5adf34632a36d6ddde5c0cb8e22439c74be71
Markus Sonderegger <marraison@gmail.com>
2016-03-16 10:34:50 +0100

    add copyright notice

--- crawler.go
+++ crawler.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Markus Sonderegger. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package crawler provides a crawler implementation.
 package crawler
 
